d7952f6978c4908b671a00483a8087de630daed0
Duhoon <eclip6@naver.com>
2019-08-13 11:27:01 +0900

    hashmap

--- test_java/hashmap.java
+++ test_java/hashmap.java
@@ -5,7 +5,7 @@ public class hashmap {
         HashMap<Integer, String> hmap = new HashMap<Integer, String>();
         hmap.put(12, "chaitanya") ; hmap.put(2, "Rahul");
         hmap.put(7, "Singh"); hmap.put(49, "Ajeet"); hmap.put(3, "Anuj");
-        Set set = hmap.entrySet();
+        /*Set set = hmap.entrySet();
         Iterator iterator = set.iterator();
         while(iterator.hasNext()){
             Map.Entry mentry = (Map.Entry)iterator.next();
@@ -14,6 +14,7 @@ public class hashmap {
         for (Entry<Integer, String> entry : hmap.entrySet()){
             System.out.printf("key:" + entry.getKey() + "& Value: " + entry.getValue());
         }
+        */
         for (Integer key : hmap.keySet()){
             System.out.printf("key: " +  key + "& Value: " + hmap.get(key) + "\n");
         }
